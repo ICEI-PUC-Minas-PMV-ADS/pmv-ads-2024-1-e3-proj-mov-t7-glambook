b79e20718e8b515b75284c2aa0ae6c52c05b7872
Carolina Rabelo <carolinamorais-13@hotmail.com>
2024-04-07 19:50:41 -0300

    Tela de login

---
+++ src/glambook/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/screens/**/*.tsx', './app/components/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
