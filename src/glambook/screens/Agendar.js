9fe56a9d451191e73b0c48d7c575f3a40537f2d5
Lídia Carvalho <123324372+lidiaaacf@users.noreply.github.com>
2024-05-25 18:10:37 -0300

    Create Agendar.js
    Tela para realizar os agendamentos

---
+++ src/glambook/screens/Agendar.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { View, Text, Image, KeyboardAvoidingView, StyleSheet, Button } from "react-native";
+
+function Agendar() {
+    return (
+        <View><Text>Lorem Ipsum</Text></View>
+      );
+    }; 
+
+
+export default Agendar;
