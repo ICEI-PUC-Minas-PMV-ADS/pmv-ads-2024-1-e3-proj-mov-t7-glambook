808b23b24b5fd6b91550edac05889ac83dd8177d
Lídia Carvalho <123324372+lidiaaacf@users.noreply.github.com>
2024-05-25 18:09:56 -0300

    Create Agendamentos.js
    Tela de agendamentos

---
+++ src/glambook/screens/Agendamentos.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { View, Text, Image, KeyboardAvoidingView, StyleSheet, Button } from "react-native";
+
+function Agendamentos() {
+    return (
+        <View><Text>Lorem Ipsum</Text></View>
+      );
+    }; 
+
+
+export default Agendamentos;
